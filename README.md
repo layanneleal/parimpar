95e650a36733b26b1e9429cfcdb618663623116d
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-10-15 07:05:04 -0300

    Não finalizado

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # parimpar
- Programa Par ou Ímpar
+ **Programa Par ou Ímpar**  
+Programa que ler um número e informa se ele é par ou
+ímpar. 
---
+++ script.js
@@ -0,0 +1,33 @@
+function parImpar() {
+  //Cria referência aos elementos da página
+  let inNumero = document.getElementById('inNumero')
+  let outResposta = document.getElementById('outResposta')
+
+  //Obtem e converte o conteúdo do campo inNumero
+  let numero = Number(inNumero.value)
+  let resto = numero % 2 == 0 ? 'Par' : 'Ímpar'
+
+  outResposta.textContent = 'Resposta: ' + numero + ' é par'
+  outResposta.textContent = 'Resposta: ' + resto + ' é impar'
+
+  
+  /*
+
+  //Se não preencheu ou NaN
+  if (numero == 0 || isNaN(numero)) {
+    alert('Digite um número válido!')
+    inNumero.focus()
+    return
+  }
+ 
+/*
+  //Se o número for maior que 0 e divisível po 2
+  if (numero > 0 && numero / 2) {
+    outResposta.textContent = 'Resposta: ' + numero + ' é par'
+  } else {
+    outResposta.textContent = 'Resposta: ' + numero + 'é impar'
+}*/
+}
+//Cria referência ao elemento btVerificar e registra um evento associado a função
+let btVerificar = document.getElementById('btVerificar')
+btVerificar.addEventListener('click', parImpar)
